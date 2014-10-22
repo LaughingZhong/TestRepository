391c2eeeb3c72247745c254835a4d14c801b4e3a
LaughingZhong <zgmingyeah@yeah.net>
2014-10-22 15:30:33 +0800

    ......

--- GithubTestDemo/GithubTestDemo/AppDelegate.m
+++ GithubTestDemo/GithubTestDemo/AppDelegate.m
@@ -20,6 +20,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    NSLog(@"叼叼你");
     return YES;
 }
 
