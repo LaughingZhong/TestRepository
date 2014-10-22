59d3f7506da73030e466a986f597c31b43846832
LaughingZhong <zgmingyeah@yeah.net>
2014-10-22 15:21:45 +0800

    test

---
+++ GithubTestDemo/GithubTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubTestDemo
+//
+//  Created by LaughingZhong on 14/10/22.
+//  Copyright (c) 2014年 Laughing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
